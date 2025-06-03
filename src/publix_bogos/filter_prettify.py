d1959f2f834bd91b73b6c5dbc0853f4662561709
superjosue <5185592+superjosue@users.noreply.github.com>
2025-06-03 17:14:20 -0400

    Refactor codebase for simplicity

--- src/publix_bogos/filter_prettify.py
+++ src/publix_bogos/filter_prettify.py
@@ -14,16 +14,15 @@ def filter_prettify_items(bogo_items: list[BogoItem], keywords: list[str], prefi
     Returns:
         list[str]: list of filtered bogo items that have prefixing and postfixing applied.
     """
-    filtered_prettified_bogo_items = list[str]()
+    results: list[str] = []
     for item in bogo_items:
-        if is_any_whole_word_in_text(item.name, keywords):
-            item_text = f'{item.name} is {item.type.value} {item.effective_dates}'
-            if prefix:
-                item_text = f'{prefix} {item_text}'
+        if not is_any_whole_word_in_text(item.name, keywords):
+            continue
+        text = f"{item.name} is {item.type.value} {item.effective_dates}"
+        if prefix:
+            text = f"{prefix} {text}"
+        if postfix:
+            text = f"{text} {postfix}"
+        results.append(text)
 
-            if postfix:
-                item_text = f'{item_text} {postfix}'
-
-            filtered_prettified_bogo_items.append(item_text)
-
-    return filtered_prettified_bogo_items
+    return results
